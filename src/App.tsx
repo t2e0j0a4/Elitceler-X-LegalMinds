c97255689fb76ae5b7217fe41400c4a0e40b7e3c
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-09-04 21:32:23 +0530

    app reset

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+// Components
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+// Pages
+import Home from './pages/Home/Home';
+import Consultation from './pages/Consultation/Consultation';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
+
+// React Router Dom
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <div className='app__main'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/consultation/:name' element={<Consultation/>}/>
+          <Route path='/error' element={<PageNotFound/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
